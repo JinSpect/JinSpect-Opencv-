1175186dd40d71345343fc53612c133d08ac13df
JinSpect <user@DESKTOP-AI5IPQK>
2025-07-30 16:52:34 +0900

    7030오후 디자인 변화

---
+++ JinSpect/Property/FlipProp.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace JinSpect.Property
+{
+    partial class FlipProp
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xFlip = new System.Windows.Forms.RadioButton();
+            this.yFlip = new System.Windows.Forms.RadioButton();
+            this.xyFlip = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // xFlip
+            // 
+            this.xFlip.AutoSize = true;
+            this.xFlip.Location = new System.Drawing.Point(16, 24);
+            this.xFlip.Name = "xFlip";
+            this.xFlip.Size = new System.Drawing.Size(69, 22);
+            this.xFlip.TabIndex = 8;
+            this.xFlip.TabStop = true;
+            this.xFlip.Text = "수평";
+            this.xFlip.UseVisualStyleBackColor = true;
+            // 
+            // yFlip
+            // 
+            this.yFlip.AutoSize = true;
+            this.yFlip.Location = new System.Drawing.Point(16, 52);
+            this.yFlip.Name = "yFlip";
+            this.yFlip.Size = new System.Drawing.Size(69, 22);
+            this.yFlip.TabIndex = 8;
+            this.yFlip.TabStop = true;
+            this.yFlip.Text = "수직";
+            this.yFlip.UseVisualStyleBackColor = true;
+            // 
+            // xyFlip
+            // 
+            this.xyFlip.AutoSize = true;
+            this.xyFlip.Location = new System.Drawing.Point(16, 80);
+            this.xyFlip.Name = "xyFlip";
+            this.xyFlip.Size = new System.Drawing.Size(114, 22);
+            this.xyFlip.TabIndex = 8;
+            this.xyFlip.TabStop = true;
+            this.xyFlip.Text = "수평+수직";
+            this.xyFlip.UseVisualStyleBackColor = true;
+            // 
+            // FlipProp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.xyFlip);
+            this.Controls.Add(this.yFlip);
+            this.Controls.Add(this.xFlip);
+            this.Name = "FlipProp";
+            this.Size = new System.Drawing.Size(261, 271);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton xFlip;
+        private System.Windows.Forms.RadioButton yFlip;
+        private System.Windows.Forms.RadioButton xyFlip;
+    }
+}
---
+++ JinSpect/Property/PyramidProp.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace JinSpect.Property
+{
+    partial class PyramidProp
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUP = new System.Windows.Forms.RadioButton();
+            this.btnDown = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnUP
+            // 
+            this.btnUP.AutoSize = true;
+            this.btnUP.Location = new System.Drawing.Point(27, 23);
+            this.btnUP.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUP.Name = "btnUP";
+            this.btnUP.Size = new System.Drawing.Size(54, 22);
+            this.btnUP.TabIndex = 0;
+            this.btnUP.TabStop = true;
+            this.btnUP.Text = "Up";
+            this.btnUP.UseVisualStyleBackColor = true;
+            // 
+            // btnDown
+            // 
+            this.btnDown.AutoSize = true;
+            this.btnDown.Location = new System.Drawing.Point(27, 53);
+            this.btnDown.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(79, 22);
+            this.btnDown.TabIndex = 0;
+            this.btnDown.TabStop = true;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            // 
+            // PyramidProp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUP);
+            this.Name = "PyramidProp";
+            this.Size = new System.Drawing.Size(220, 256);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton btnUP;
+        private System.Windows.Forms.RadioButton btnDown;
+    }
+}
